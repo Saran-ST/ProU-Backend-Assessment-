fb35235ddb1c3be4e6f064ee87b4e61d97a679ae
Saran-ST <stsaran2004@gmail.com>
2025-11-14 22:49:38 +0530

    cleaned the code files

--- users/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.contrib import admin
-from .models import UserProfile
-
-@admin.register(UserProfile)
-class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'full_name', 'location')
-    search_fields = ('user__username', 'full_name', 'location')
