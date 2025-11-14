cb73e19c65037545f66256501ec03ab8344ef5e1
Saran-ST <stsaran2004@gmail.com>
2025-11-14 22:53:48 +0530

    Finalize project cleanup, remove unused users app, organize structure

--- events/views.py
+++ events/views.py
@@ -5,9 +5,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 
-# ---------------------------
 # EMPLOYEES LIST + CREATE
-# ---------------------------
 class EmployeeListCreateView(generics.ListCreateAPIView):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
@@ -21,22 +19,18 @@ class EmployeeListCreateView(generics.ListCreateAPIView):
     ]
 
 
-# ---------------------------
 # EMPLOYEE DETAIL (GET/PUT/DELETE)
-# ---------------------------
 class EmployeeDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
 
 
-# ---------------------------
 # TASKS LIST + CREATE
-# ---------------------------
 class TaskListCreateView(generics.ListCreateAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
 
-    # BONUS FEATURES
+    #sorting fiters
     filterset_fields = ['status', 'employee']
     search_fields = ['title', 'description']
     ordering_fields = ['created_at', 'due_date']
@@ -46,18 +40,13 @@ class TaskListCreateView(generics.ListCreateAPIView):
     ]
 
 
-# ---------------------------
 # TASK DETAIL (GET/PUT/DELETE)
-# ---------------------------
 class TaskDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
 
 
 class DashboardStatsView(APIView):
-    """
-    Dashboard analytics for employees and tasks.
-    """
     def get(self, request):
         total_employees = Employee.objects.count()
         total_tasks = Task.objects.count()
@@ -73,5 +62,4 @@ class DashboardStatsView(APIView):
             "pending_tasks": pending_tasks,
             "in_progress_tasks": in_progress_tasks,
         }
-
         return Response(data)
